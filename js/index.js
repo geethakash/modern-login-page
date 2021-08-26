0ff8b163af345fc270e896bc4da7ec8c21d0487a
GeetH AkasH <58387103+geethakash@users.noreply.github.com>
2021-08-26 12:33:31 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
- 
+var val = 0;
+
+document.getElementById("drkmodebtn").addEventListener('click', function () {
+    if (val == 0) {
+        val = 1;
+        document.getElementById('body').classList.add("light-theme");
+        document.getElementById('body').classList.remove("dark-theme");
+
+
+    }
+    else {
+        val = 0;
+        document.getElementById('body').classList.add("dark-theme");
+        document.getElementById('body').classList.remove("light-theme");
+
+    
+    } 
+    console.log("clicked");
+}
+)
